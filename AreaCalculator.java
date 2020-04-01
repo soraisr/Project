1eaa8033a7149e7a25e01d3b17365c72172b38ad
aviadbd <aviad.bendov@gmail.com>
2020-04-02 01:34:18 +0300

    Create AreaCalculator.java
    To give you a head start. ;)

---
+++ AreaCalculator.java
@@ -0,0 +1,22 @@
+public class AreaCalculator {
+  public static double calculate(String... params) { return 0.0; }
+  
+  public static void main(String[] args) {
+    if (AreaCalculator.calculate("square", "3", "20") != 60.0) {
+      System.out.println("Wrong");
+      return;
+    }
+    
+    if (Math.ceil(AreaCalculator.calculate("circle", "5")) != 79.0) {
+      System.out.println("Wrong");
+      return;
+    }
+    
+    if (Math.ceil(AreaCalculate.calculate("triangle", "10", "5", "20")) != 73.0) {
+      System.out.println("Wrong");
+      return;
+    }
+    
+    System.out.println("Good! For now..");
+  }
+}
