f327510d7086785d373030e8558626aea8fcf877
Liraz <Liraz@DESKTOP-D1N3D3L>
2020-04-12 12:31:15 +0300

    errors in test

---
+++ NotEnoughParamException.java
@@ -0,0 +1,23 @@
+
+public class NotEnoughParamException extends RuntimeException {
+	
+	      // Parameterless Constructor
+	        public NotEnoughParamException() {
+	    
+	      }
+
+	      // Constructor that accepts a message
+	      public  NotEnoughParamException(String message)
+	      {
+	         super(message);
+	      }
+	      public   NotEnoughParamException(String message ,Throwable cause)
+	      {
+	         super(message , cause );
+	      }
+	      public   NotEnoughParamException(Throwable cause)
+	      {
+	         super( cause );
+	      }
+
+	 }
