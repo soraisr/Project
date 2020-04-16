1a7b6cd1c47cfd524650ca3b778d420fe117900b
Liraz <Liraz@DESKTOP-D1N3D3L>
2020-04-16 12:40:27 +0300

    4.5 level 2 rectBypoints

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 import java.lang.reflect.Constructor;
+import java.util.regex.Matcher;  
+import java.util.regex.Pattern; 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Scanner;
 
 
 import shape.*;
@@ -17,19 +20,52 @@ public class Main {
 		return (Shape) ctor.newInstance(inputs[0] , inputs [1] , inputs [2]);
 	}
 	
-	public static double calculate(String... params) throws ClassNotFoundException { 
+	public static double calaculateByPoints(String... params) throws NoSuchMethodException, SecurityException, ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		int n = params.length -1 ;
+		double [] x_axis = new double [n] ;
+		double [] 	y_axis = new double [n] ;
+		for (int i = 0 ; i < n ; i++)
+		{	
+	        Pattern pattern = Pattern.compile("(\\-*\\d+),\\s(\\-*\\d+)");
+	        Matcher matcher = pattern.matcher(params[i+1]);
+	        if (matcher.find())
+	        {
+	        x_axis[i] = Double.parseDouble(matcher.group(1));
+	        y_axis[i] = Double.parseDouble(matcher.group(2));
+	        }
+	        else
+	        	throw new NumberFormatException();
+		}
+		Class [] inputArrayType = new Class [2] ;
+		inputArrayType[0] =x_axis.getClass();
+		inputArrayType[1] =y_axis.getClass() ;
+		Constructor<?> ctor = Class.forName("shape." +params[0]).getDeclaredConstructor(inputArrayType) ;
+		ctor.setAccessible(true);
+		Shape s = (Shape) ctor.newInstance(x_axis , y_axis);
+		return s.calculate() ;
+
+	}
+	
+	
+	public static double calculate(String... params) throws ClassNotFoundException, InstantiationException{ 
 		int n = params.length -1 ;
 		double [] inputs = new double [n] ;
-		Class[] ctorArgs = new Class [n] ; 
+		Class[] ctorArgs = new Class [n] ;
+		try {
 		for (int i = 0 ; i < n ; i++)
 		{
-			inputs[i] = Double.parseDouble(params[i+1]);
+			try {
+				inputs[i] = Double.parseDouble(params[i+1]);
+			} 
+			catch (Exception e) 
+			{
+					return calaculateByPoints(params);
+			}
 			ctorArgs[i] = double.class ;
 			if (inputs[i]<0)
 				throw new NegativeLengthException("the " + i + " input is negative !" ) ;
 		}
 
-		try {
 				Constructor<?> ctor = Class.forName("shape." +params[0]).getDeclaredConstructor(ctorArgs) ;
 				ctor.setAccessible(true);
 				Class [] inputArrayType = new Class [2] ;
@@ -77,7 +113,11 @@ public static void main(String[] args)
 		 System.out.println("Wrong3");
 		 return;
 	  }
-	
+	  if (Math.ceil(Main.calculate("RectByPoints", "(1, 2)", "(-1, 3)")) != 2.0) {
+
+		 System.out.println("Wrong4");
+		 return;
+	  }
 	  
 	  System.out.println("Good! For now..");
 	    }
--- shape/RectByPoints.java
+++ shape/RectByPoints.java
@@ -1,10 +1,13 @@
 package shape;
 
-public class RectByPoints implements Shap {
+public class RectByPoints implements Shape {
    private Rect rect; // This is called Delegation
 
-   public RectByPoints(int[] a, int[] b) {
-     this.rect = new Rect(5, 10); // this should be implemented for real some time :)
+   public RectByPoints(double[] a, double[] b) {
+	  if (a.length==2 && b.length==2)
+		  this.rect = new Rect( Math.abs(a[1]-a[0]),  Math.abs(b[1]-b[0])); // this should be implemented for real some time :)
+	  else 
+		  this.rect = null ;
    }
   
    public double calculate() {
