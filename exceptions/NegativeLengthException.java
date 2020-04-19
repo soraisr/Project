c8337bfa7d91c1ece599f977f31fe285391fb362
soraisr <63023072+soraisr@users.noreply.github.com>
2020-04-19 23:04:15 +0300

    Update NegativeLengthException.java

--- exceptions/NegativeLengthException.java
+++ exceptions/NegativeLengthException.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD:NegativeLengthException.java
-<<<<<<< HEAD
 public class NegativeLengthException extends RuntimeException {
     // Parameterless Constructor
     public NegativeLengthException() {
@@ -20,50 +18,4 @@ public class NegativeLengthException extends RuntimeException {
 	     super( cause );
   }
 }
-=======
-public class NegativeLengthException extends RuntimeException {
-    // Parameterless Constructor
-    public NegativeLengthException() {
 
-    }
-
-  // Constructor that accepts a message
-  public  NegativeLengthException(String message)
-  {
-     super(message);
-  }
-  public  NegativeLengthException(String message ,Throwable cause)
-  {
-     super(message , cause );
-  }
-  public  NegativeLengthException(Throwable cause)
-  {
-	     super( cause );
-  }
-
-}
->>>>>>> 1bff33f15d8f9bcd9f8eee57902a555b462b5b2c
-=======
-package exceptions;
-public class NegativeLengthException extends RuntimeException {
-    // Parameterless Constructor
-    public NegativeLengthException() {
-
-    }
-
-  // Constructor that accepts a message
-  public  NegativeLengthException(String message)
-  {
-     super(message);
-  }
-  public  NegativeLengthException(String message ,Throwable cause)
-  {
-     super(message , cause );
-  }
-  public  NegativeLengthException(Throwable cause)
-  {
-	     super( cause );
-  }
-
-}
->>>>>>> a3b309c971d046486f55d69c803ca46685040847:exceptions/NegativeLengthException.java
