f80c6e20563b10b6b2d17d1eb465ee6b3a78622b
soraisr <63023072+soraisr@users.noreply.github.com>
2020-04-20 00:29:37 +0300

    Update NotExistsShapeException.java

--- exceptions/NotExistsShapeException.java
+++ exceptions/NotExistsShapeException.java
@@ -1,28 +1,4 @@
-<<<<<<< HEAD
 
-public class NotExistsShapeException extends RuntimeException {
-	
-    // Parameterless Constructor
-      public NotExistsShapeException() {
-  
-    }
-
-    // Constructor that accepts a message
-    public  NotExistsShapeException(String message)
-    {
-       super(message);
-    }
-    public  NotExistsShapeException(String message ,Throwable cause)
-    {
-       super(message , cause );
-    }
-    public  NotExistsShapeException(Throwable cause)
-    {
-       super(cause);
-    }
-}
-
-=======
 package exceptions;
 
 public class NotExistsShapeException extends RuntimeException {
@@ -48,4 +24,3 @@ public class NotExistsShapeException extends RuntimeException {
 
 
 }
->>>>>>> 5a124e231bf95c88c73f6a52a47c8239bfa66296
