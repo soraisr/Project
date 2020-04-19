5a6eb0f0ddf76cdeb542cbd87dafc4231825a823
soraisr <63023072+soraisr@users.noreply.github.com>
2020-04-19 23:05:36 +0300

    Update NotEnoughParamException.java

--- exceptions/NotEnoughParamException.java
+++ exceptions/NotEnoughParamException.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD:NotEnoughParamException.java
-<<<<<<< HEAD
 
 public class NotEnoughParamException extends RuntimeException {
 	
@@ -22,58 +20,7 @@ public class NotEnoughParamException extends RuntimeException {
 	         super( cause );
 	      }
 }
-=======
-
-public class NotEnoughParamException extends RuntimeException {
-	
-	      // Parameterless Constructor
-	        public NotEnoughParamException() {
-	    
-	      }
-
-	      // Constructor that accepts a message
-	      public  NotEnoughParamException(String message)
-	      {
-	         super(message);
-	      }
-	      public   NotEnoughParamException(String message ,Throwable cause)
-	      {
-	         super(message , cause );
-	      }
-	      public   NotEnoughParamException(Throwable cause)
-	      {
-	         super( cause );
-	      }
-
-	 }
 
 
->>>>>>> 1bff33f15d8f9bcd9f8eee57902a555b462b5b2c
-=======
-package exceptions;
-
-public class NotEnoughParamException extends RuntimeException {
-	
-	      // Parameterless Constructor
-	        public NotEnoughParamException() {
-	    
-	      }
-
-	      // Constructor that accepts a message
-	      public  NotEnoughParamException(String message)
-	      {
-	         super(message);
-	      }
-	      public   NotEnoughParamException(String message ,Throwable cause)
-	      {
-	         super(message , cause );
-	      }
-	      public   NotEnoughParamException(Throwable cause)
-	      {
-	         super( cause );
-	      }
-
-	 }
 
 
->>>>>>> a3b309c971d046486f55d69c803ca46685040847:exceptions/NotEnoughParamException.java
